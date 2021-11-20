2fce44fe1474e2fd5ae72503c016c9dbcbabbeee
Sylvie-B <unsolo@hotmail.fr>
2021-11-21 00:40:40 +0100

    mise en page C + backSpace

--- appCalc.js
+++ appCalc.js
@@ -1,6 +1,8 @@
 let frame = document.getElementsByTagName('span');
 let screen = document.getElementById('screen');
 let equal = document.getElementById('equal');
+let itemFrame = document.getElementById('container').getElementsByTagName('div');
+
 
 for(let i = 0 ; i < frame.length ; i++){
     frame[i].addEventListener('click', function (){
@@ -9,6 +11,31 @@ for(let i = 0 ; i < frame.length ; i++){
 }
 
 equal.addEventListener('click', function (){
-    screen.innerHTML = eval(screen.innerText);
+    let result = eval(screen.innerText);
+    if(isNaN(result)){
+        screen.innerHTML = "error";
+    }
+    else {
+        screen.innerHTML = result.toString();
+    }
+
 })
+
+let item;
+
+for (let i = 0 ; i < itemFrame.length; i++){
+    itemFrame[i].addEventListener('click', function (){
+        console.log(i);
+        switch (i){
+            case 1 :
+                screen.innerHTML = "";
+                break;
+            case 2 :
+                screen.innerHTML += itemFrame[i].innerHTML;
+                break;
+            case 3 :
+                screen.innerHTML = screen.innerHTML.slice(0, -1);
+        }
+    })
+}
 
