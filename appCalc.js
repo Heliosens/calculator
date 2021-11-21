007ed8abc588d3e6fb668758516509168ba8074a
Sylvie-B <unsolo@hotmail.fr>
2021-11-21 01:32:22 +0100

    comments

--- appCalc.js
+++ appCalc.js
@@ -1,41 +1,41 @@
+// get touch
 let frame = document.getElementsByTagName('span');
 let screen = document.getElementById('screen');
-let equal = document.getElementById('equal');
 let itemFrame = document.getElementById('container').getElementsByTagName('div');
 
-
+// listen touch to write on screen
 for(let i = 0 ; i < frame.length ; i++){
-    frame[i].addEventListener('click', function (){
-        screen.innerHTML += frame[i].innerHTML;
-    })
+    frame[i].addEventListener('click', ()=>screen.innerHTML += frame[i].innerHTML)
 }
 
-equal.addEventListener('click', function (){
-    let result = eval(screen.innerText);
-    if(isNaN(result)){
-        screen.innerHTML = "error";
-    }
-    else {
-        screen.innerHTML = result.toString();
-    }
-
-})
-
-let item;
-
+// listen frame to calcul
+let result =0;
 for (let i = 0 ; i < itemFrame.length; i++){
     itemFrame[i].addEventListener('click', function (){
-        console.log(i);
         switch (i){
-            case 1 :
+            case 1 : // C
                 screen.innerHTML = "";
                 break;
-            case 2 :
-                screen.innerHTML += itemFrame[i].innerHTML;
+            case 2 : // Ans
+                if (isNaN(result)){
+                    screen.innerHTML += "";
+                }
+                else {
+                    screen.innerHTML += result;
+                }
                 break;
-            case 3 :
+            case 3 : // backspace
                 screen.innerHTML = screen.innerHTML.slice(0, -1);
+                break;
+            case 4 : // equal
+                result = eval(screen.innerText);
+                if(isNaN(result)){
+                    screen.innerHTML = "error";
+                }
+                else {
+                    screen.innerHTML = result.toString();
+                }
+                break;
         }
     })
 }
-
